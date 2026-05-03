3be635458a076ee2b864667cb005a53d399e6dd3
Micco <miccoasigo@gmail.com>
2026-05-03 17:16:33 +0300

    edited

--- router/location.js
+++ router/location.js
@@ -6,11 +6,12 @@ router.post("/api/location",async(req,res)=>{
     const {body:{region,districts}} = req;
     const location = new LocationModel({region,districts})
     try{
-        await Location.save();
-        res.status(200).send({
+      const data =   await Location.save();
+        // res.status(200).send({
             success:true,
             message:"succesfully saved"
-        })
+        // })
+        res.status(201).send(data);
     }catch(e){
         res.status(500).send({
             success:true,
